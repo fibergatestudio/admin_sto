876c61e2dbf0925879963cfa00464c5bba73ac1a
Zhenya <krivorotzena60@gmail.com>
2019-01-18 16:44:57 +0200

    modify method client

--- app/Http/Controllers/Client_Controller.php
+++ app/Http/Controllers/Client_Controller.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Client;
 use Auth;
 use App\Cars_in_service;
 
@@ -10,7 +11,10 @@ class Client_Controller extends Controller
     public function client()
     {
         $id = Auth::id();
-        $car = Cars_in_service::where('owner_client_id',$id)->get();
-        return view('client.client')->with(array('cars'=>$car));
+        $owner_id = Client::where('user_id',$id)->pluck('id');
+        if(!$owner_id->isEmpty()) {
+            $car = Cars_in_service::where('owner_client_id', $owner_id[0])->get();
+            return view('client.client')->with(array('cars' => $car));
+        }
     }
 }
