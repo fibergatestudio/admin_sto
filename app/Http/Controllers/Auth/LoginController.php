6c21c8a36f8a49c2bb52a758beddd4e3e984b2b9
Zhenya <krivorotzena60@gmail.com>
2019-01-18 14:53:19 +0200

    in Login Contr add method logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,6 +37,10 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
+    public function logout(Request $request) {
+        Auth::logout();
+        return redirect('/');
+    }
     // CUSTOM
 
     public function username()
