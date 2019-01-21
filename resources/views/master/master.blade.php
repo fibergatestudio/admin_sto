01b396cde971274d8361cc05f1373a34f23c2962
Zhenya <krivorotzena60@gmail.com>
2019-01-21 17:32:05 +0200

    add view master

---
+++ resources/views/master/master.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.limitless')
+@section('page_name')
+    Сторінка мастера
+@endsection
+@section('content')
+    <div class="container">
+        <div class="card" style="margin-bottom: 20px">
+            <div class="card-body" style="text-align: center">
+            </div>
+        </div>
+    </div>
+@endsection
