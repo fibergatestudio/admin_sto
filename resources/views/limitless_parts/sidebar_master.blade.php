f6c82428a5de2855035010141d8128e6cf932ac5
Zhenya <krivorotzena60@gmail.com>
2019-01-22 13:25:43 +0200

    modify master sidebar

--- resources/views/limitless_parts/sidebar_master.blade.php
+++ resources/views/limitless_parts/sidebar_master.blade.php
@@ -4,6 +4,25 @@
 	<ul class="nav nav-sidebar" data-nav-type="accordion">
 		<!-- Main -->
 		<li class="nav-item-header"><div class="text-uppercase font-size-xs line-height-xs">Меню</div> <i class="icon-menu" title="Main"></i></li>
+
+		<li class="nav-item">
+			<a class="nav-link" href="{{ url('/assignments ') }}">
+				<i class="icon-list"></i>
+				<span>
+								Управление нарядами
+							</span>
+			</a>
+		</li>
+
+		<li class="nav-item">
+			<a class="nav-link" href="{{ url('/employees') }}">
+				<i class="icon-list"></i>
+				<span>
+								Просмотр профилей
+							</span>
+			</a>
+		</li>
+
 		<li class="nav-item">
 			<a class="nav-link" href="{{ url('/logout') }}"
 			   onclick="event.preventDefault();
