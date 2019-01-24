d888da95ee2e4e21a93ee23a55c13cbd378b8c95
Zhenya <krivorotzena60@gmail.com>
2019-01-25 00:46:31 +0200

    modify view client

--- resources/views/client/client.blade.php
+++ resources/views/client/client.blade.php
@@ -1,16 +1,30 @@
 @extends('layouts.limitless')
 @section('page_name')
-    Список машин клієнта
+    Список машин клиента
 @endsection
 @section('content')
-    <div class="container">
+    <div class="container"  >
         @if(!$cars->isEmpty())
             @foreach ($cars as $car)
-                <div class="card" style="margin-bottom: 20px">
+                <div class="card" style="margin-bottom: 20px;margin-top: 10px">
                     <div class="card-body" style="text-align: center">
-                        {{($car['general_name'])}}
+                        {{($car->general_name)}}
                     </div>
                 </div>
+
+                {{-- Наряды клиента : кнопка --}}
+                <a href="{{ url('client/assignments/'.$car->id) }}">
+                    <div class="btn btn-success">
+                        Активные наряды
+                    </div>
+                </a>
+
+                {{-- Архив : переход --}}
+                <a href="{{ url('client/assignments_archive/'.$car->id) }}">
+                    <div class="btn btn-light">
+                        Архивные наряды
+                    </div>
+                </a>
             @endforeach
         @else
             <div class="card">
