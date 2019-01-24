d1cde823f06a1ea17e6431592dc7c1f6fbf8a7cc
Zhenya <krivorotzena60@gmail.com>
2019-01-25 00:27:14 +0200

    add methods to Client_controller

--- app/Http/Controllers/Client_Controller.php
+++ app/Http/Controllers/Client_Controller.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Assignment;
 use App\Client;
 use Auth;
 use App\Employee;
+use App\Sub_assignment;
 use App\Cars_in_service;
 
 class Client_Controller extends Controller
@@ -18,7 +20,73 @@ class Client_Controller extends Controller
             return view('client.client')->with(array('cars' => $car));
         }
     }
+    /* Главная страница активных нарядов клиента */
+    public function assignments($id)
+    {
 
+        $assignments = Assignment::where('car_id', $id)
+            ->join('employees', 'assignments.responsible_employee_id', '=', 'employees.id')
+            ->join('cars_in_service', 'assignments.car_id', '=', 'cars_in_service.id')
+            ->select(
+                'assignments.*',
+                'employees.general_name AS employee_name',
+                'cars_in_service.general_name AS car_name'
+            )
+            ->get();
+        $assignments = $assignments->reject(function($element) {
+            return $element->status == 'archived'; //return assignments where status=='active'
+        });
+
+        if(!$assignments->isEmpty()) {
+            return view('client.assignments')->with(array('assignments' => $assignments));
+        }
+
+    }
+
+    /* Главная страница зональных нарядов клиента */
+    public function sub_assignments($id)
+    {
+
+        $sub_assignments =
+            Sub_assignment::where('assignment_id', $id)->join('workzones', 'sub_assignments.workzone_id', '=', 'workzones.id')->select('sub_assignments.*', 'workzones.general_name')->get();
+
+        foreach($sub_assignments as $sub_assignment){
+            $sub_assignment->responsible_employee = Employee::find($sub_assignment->responsible_employee_id)->general_name;
+        }
+
+        return view('client.sub_assignments')->with(array('sub_assignments' => $sub_assignments));
+    }
+
+    /* Главная страница архивных нарядов клиента */
+    public function assignments_archive($id)
+    {
+
+        $assignments = Assignment::where('car_id', $id)
+            ->join('employees', 'assignments.responsible_employee_id', '=', 'employees.id')
+            ->join('cars_in_service', 'assignments.car_id', '=', 'cars_in_service.id')
+            ->select(
+                'assignments.*',
+                'employees.general_name AS employee_name',
+                'cars_in_service.general_name AS car_name'
+            )
+            ->get();
+        $assignments = $assignments->reject(function($element) {
+            return $element->status == 'active'; //return assignments where status=='archived'
+        });
+
+        if(!$assignments->isEmpty()) {
+            $empty = 0;
+            return view('client.assignments_archiv')->with(array('assignments_archiv' => $assignments,'empty'=>$empty));
+        }
+        else
+        {
+            $empty = 1;
+            return view('client.assignments_archiv')->with(array('empty'=>$empty));
+        }
+
+
+
+    }
 
     public function master()
     {
