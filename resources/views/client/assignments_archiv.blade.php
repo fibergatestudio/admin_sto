5353c176bf69c2558dac46ca48f04a06c16b2dbc
Zhenya <krivorotzena60@gmail.com>
2019-01-25 13:30:27 +0200

    modify view assign archiv

--- resources/views/client/assignments_archiv.blade.php
+++ resources/views/client/assignments_archiv.blade.php
@@ -3,30 +3,38 @@
     Архив нарядов клиента
 @endsection
 @section('content')
-    <table class="table">
-        <thead>
-        <tr>
-            <th>Дата создания</th>
-            <th>Название</th>
-            <th>Ответственный работник</th>
-            <th>Авто</th>
-            <th></th>
-        </tr>
-        </thead>
-        @foreach($assignments_archiv as $assignment)
+    @if($empty==0)
+        <table class="table">
+            <thead>
             <tr>
-                <td>{{($assignment->date_of_creation)}} {{-- Дата создания --}}</td>
-                <td>{{($assignment->description)}} {{-- Описание наряда --}}</td>
-                <td>{{($assignment->employee_name)}} {{-- Ответственный работник --}}</td>
-                <td>{{($assignment->car_name)}} {{-- Назва авто --}}</td>
-                <td>
-                    <a href="{{ url('/client/sub_assignments/'.$assignment->id) }}">
-                        <div class="btn btn-secondary">
-                            Архив зональных нарядов
-                        </div>
-                    </a>
-                </td>
+                <th>Дата создания</th>
+                <th>Название</th>
+                <th>Ответственный работник</th>
+                <th>Авто</th>
+                <th></th>
             </tr>
-        @endforeach
-    </table>
+            </thead>
+            @foreach($assignments_archiv as $assignment)
+                <tr>
+                    <td>{{($assignment->date_of_creation)}} {{-- Дата создания --}}</td>
+                    <td>{{($assignment->description)}} {{-- Описание наряда --}}</td>
+                    <td>{{($assignment->employee_name)}} {{-- Ответственный работник --}}</td>
+                    <td>{{($assignment->car_name)}} {{-- Назва авто --}}</td>
+                    <td>
+                        <a href="{{ url('/client/sub_assignments/'.$assignment->id) }}">
+                            <div class="btn btn-secondary">
+                                Архив зональных нарядов
+                            </div>
+                        </a>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+    @else
+        <div class="card">
+            <div class="card-body" style="text-align: center">
+                Архивных нарядов нет
+            </div>
+        </div>
+    @endif
 @endsection
