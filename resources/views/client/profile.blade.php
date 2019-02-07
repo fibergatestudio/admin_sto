f2f0a23961b0e21f79c4a8e3542570d95e8267da
Zhenya <krivorotzena60@gmail.com>
2019-02-07 13:49:37 +0200

    new view profile

--- resources/views/client/profile.blade.php
+++ resources/views/client/profile.blade.php
@@ -1,57 +1,40 @@
 @extends('layouts.limitless')
 @section('page_name')
-    Список машин клиента
+    Профиль
 @endsection
 @section('content')
     <table class="table">
         <thead>
         <tr>
-
-            <th>Модель</th>
+            <th>Имя</th>
             <th></th>
-            <th>Брэнд</th>
+            <th>ФИО</th>
             <th></th>
-            <th>Возраст машины</th>
+            <th>Организация</th>
             <th></th>
+            <th>Телефон</th>
             <th></th>
+            <th>Баланс</th>
+            <th></th>
+            <th>Личная скидка</th>
         </tr>
         </thead>
 
-        @if(!$cars->isEmpty())
-            @foreach ($cars as $car)
                 <tr>
-
-                    <td>{{($car->car_model->model)}} {{--  Статус --}}</td>
+                    <td>{{($profile->general_name)}} {{--  Статус --}}</td>
                     <td></td>
-                    <td>{{($car->car_model->brand)}} {{--  Статус --}}</td>
+                    <td>{{($profile->fio)}} {{--  Статус --}}</td>
                     <td></td>
-                    <td>{{($car->release_year)}} {{-- Ответственный работник --}}</td>
+                    <td>{{($profile->organization)}} {{-- Ответственный работник --}}</td>
+                    <td></td>
+                    <td>{{($profile->phone)}} {{-- Ответственный работник --}}</td>
+                    <td></td>
+                    <td>{{($profile->balance)}} {{-- Ответственный работник --}}</td>
+                    <td></td>
+                    <td>{{($profile->discount)}} {{-- Ответственный работник --}}</td>
 
-                    <td>
-                        {{-- Наряды клиента : кнопка --}}
-                        <a href="{{ url('client/assignments/'.$car->id) }}">
-                            <div class="btn btn-success">
-                                Активные наряды
-                            </div>
-                        </a>
-                    </td>
-                    <td>
-                        {{-- Архив : переход --}}
-                        <a href="{{ url('client/assignments_archive/'.$car->id) }}">
-                            <div class="btn btn-light">
-                                Архивные наряды
-                            </div>
-                        </a>
-                    </td>
                 </tr>
-            @endforeach
-        @else
-            <div class="card">
-                <div class="card-body" style="text-align: center">
-                    Машин даного клієнта поки нема
-                </div>
-            </div>
-        @endif
+
 
     </table>
 @endsection
