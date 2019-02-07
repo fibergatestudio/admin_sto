5cd5108daa1f72cf600e1f42724455223ab6530f
Zhenya <krivorotzena60@gmail.com>
2019-02-07 13:57:19 +0200

    mod

--- app/Http/Controllers/Client_Controller.php
+++ app/Http/Controllers/Client_Controller.php
@@ -31,21 +31,9 @@ class Client_Controller extends Controller
         })->get();
 
         return view('client.client')->with(array('cars' => $cars));
-        //dd(Car_model_list::whereIn('general_name',$a)->get());
-       /* $id = Auth::id();
-        $owner_id = Client::where('user_id',$id)->pluck('id');
-        if(!$owner_id->isEmpty()) {
-            $car = Cars_in_service::where('owner_client_id', $owner_id[0])->get();
-            return view('client.client')->with(array('cars' => $car));
-        }*/
-    }
-
-    public function balance()
-    {
-        //$profile = ;
-        //return view('client.client')->with(['profile' => $profile]);
 
     }
+
 
     public function profile()
     {
