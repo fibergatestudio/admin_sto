9aeda5c78b6d688508992688107801d6790861c2
Zhenya <krivorotzena60@gmail.com>
2019-02-17 23:36:38 +0200

    modify

--- resources/views/master/assignments.blade.php
+++ resources/views/master/assignments.blade.php
@@ -18,6 +18,7 @@ function beautify_date($mysql_date){
             <th scope="col">Ответственный сотрудник</th>
             <th scope="col">Авто</th>
             <th scope="col"></th>
+            <th scope="col"></th>
         </tr>
         </thead>
         @foreach($assignments as $assignment)
@@ -34,7 +35,22 @@ function beautify_date($mysql_date){
                 {{-- Машина --}}
                 <td>{{ $assignment->car_name }}</td>
 
+                @if($assignment->confirmed == 'confirmed')
+                    <td style="text-align: center">
+                        <b class="badge bg-success" type="text">{{ $assignment->confirmed }}</b><br>
+                    </td>
+                    @else
+                    <td>
+                        <a href="{{ url('/master/confirm/'.$assignment->id) }}">
+                            <div class="btn btn-warning">
+                                Подтвердить запись
+                            </div>
+                        </a>
+                    </td>
+                    @endif
+
                 {{-- Кнопка подробнее --}}
+
                 <td>
                     <a href="{{ url('/master/assignments/view/'.$assignment->id) }}">
                         <div class="btn btn-secondary">
