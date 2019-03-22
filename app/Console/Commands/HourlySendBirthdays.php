c6b90c2922704fcedab8609bd271770c0878d6ad
Sergii81 <sezik1@gmail.com>
2019-03-22 16:20:28 +0200

    send Birthdays to telegram 22-03

--- app/Console/Commands/HourlySendBirthdays.php
+++ app/Console/Commands/HourlySendBirthdays.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Command;
 use App\Employee;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
-class SendBirthday extends Command
+class HourlySendBirthdays extends Command
 {
     /**
      * The name and signature of the console command.
@@ -42,31 +42,31 @@ class SendBirthday extends Command
         $dateToday = date("m-d");
         $namesToday = ''; // имена тех у кого сегодня ДР
         $namesTomorrow = ''; // имена тех у кого завтра ДР
-        foreach(Employee::getBirthdayToday() as $employee){						
-            $namesToday .= $employee->fio.', ';									
+        foreach(Employee::getBirthdayToday() as $employee){                     
+            $namesToday .= $employee->fio.', ';                                 
         }
-        foreach(Employee::getBirthdayTomorrow() as $employee){						
-            $namesTomorrow .= $employee->fio.', ';									
+        foreach(Employee::getBirthdayTomorrow() as $employee){                      
+            $namesTomorrow .= $employee->fio.', ';                                  
         }
         $textToday = "Именинники сегодня:\n". $namesToday; // текст для телеграм
         $textTomorrow = "Именинники завтра:\n". $namesTomorrow; // текст для телеграм
         $text = $textToday."\n".$textTomorrow; // текст для телеграм
         
         // Посылка сообщений тем у кого ДР сегодня (сообщение о тех у кого ДР завтра)
-        foreach(Employee::getBirthdayToday() as $employee){						
+        foreach(Employee::getBirthdayToday() as $employee){                     
             if(!empty($employee->telegram_id)){
                 Telegram::sendMessage([
                     'chat_id' => $employee->telegram_id,
                     'parse_mode' => 'HTML',
                     'text' => $textTomorrow
-                ]);	
+                ]); 
             }else{  // если поле с данными telegram_id пустое, отсылаем в тестовый канал
                 Telegram::sendMessage([
                     'chat_id' => env('TELEGRAM_CHANNEL_ID', ''),
                     'parse_mode' => 'HTML',
                     'text' => $textTomorrow
-                ]);	
-            }									
+                ]); 
+            }                                   
         } //end foreach
         
         // Посылка сообщений всем работникам со статусом 'active', кроме именинников сегодня
@@ -77,15 +77,15 @@ class SendBirthday extends Command
                         'chat_id' => $employee->telegram_id,
                         'parse_mode' => 'HTML',
                         'text' => $text
-                    ]);	
+                    ]); 
                 }else{  // если поле с данными telegram_id пустое, отсылаем в тестовый канал
                     Telegram::sendMessage([
                         'chat_id' => env('TELEGRAM_CHANNEL_ID', ''),
                         'parse_mode' => 'HTML',
                         'text' => $text
-                    ]);	
+                    ]); 
                 }
-            }            									
+            }                                               
         } // end foreach      
     }
 }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\SendBirthday::class
+        Commands\HourlySendBirthdays::class
     ];
 
     /**
