55f3e3b7e46cb52036b277971e7c01b254abe0c7
Luidmyla Moshta <luidmyla.moshta@gmail.com>
2020-01-22 17:01:02 +0200

    color-tab 22.01.2020

--- resources/views/admin/shifts/admin_shifts_index.blade.php
+++ resources/views/admin/shifts/admin_shifts_index.blade.php
@@ -6,11 +6,11 @@
 
 @section('content')
     <h2>Смены</h2>
-    
 
-    
-    <table class="table">
-        <thead>
+
+    <div class="card card-p">
+        <table class="table">
+            <thead>
             <tr>
                 <th>Имя</th>
                 <th>Статус</th>
@@ -22,38 +22,40 @@
                 <th>Выплата</th>
                 <th></th>{{-- Кнопки управления --}}
             </tr>
-        </thead>
-        <tbody>
-        @foreach($shifts as $shift)
-        <tr>
-            <td>
-            {{ $shift->shift_emp_name }}<br>
-            </td>
-            <td>
-            {{ $shift->status }}<br>
-            </td>
-            <td>
-            {{ $shift->date }}<br>
-            </td>
-            <td>
-            {{ $shift->opened_at }}<br>
-            </td>
-            <td>
-            {{ $shift->closed_at }}<br>
-            </td>
-            <td>
-            {{ $shift->note }}<br>
-            </td>
-            <td>
-            {{ $shift->payment_amount }}<br>
-            </td>
-            <td>
-            {{ $shift->payment_status }}<br>
-            </td>
-        </tr>
-        @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+            @foreach($shifts as $shift)
+                <tr>
+                    <td>
+                        {{ $shift->shift_emp_name }}<br>
+                    </td>
+                    <td>
+                        {{ $shift->status }}<br>
+                    </td>
+                    <td>
+                        {{ $shift->date }}<br>
+                    </td>
+                    <td>
+                        {{ $shift->opened_at }}<br>
+                    </td>
+                    <td>
+                        {{ $shift->closed_at }}<br>
+                    </td>
+                    <td>
+                        {{ $shift->note }}<br>
+                    </td>
+                    <td>
+                        {{ $shift->payment_amount }}<br>
+                    </td>
+                    <td>
+                        {{ $shift->payment_status }}<br>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+
     
     
 @endsection
